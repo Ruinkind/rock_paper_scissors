7e2432e19a2a091964d84a1bd7a7df7c8f91f6c8
Ruinkind <terroth@gmail.com>
2020-12-11 22:36:52 -0700

    Add game function.  Adjust playerSelection input & comments.

--- script.js
+++ script.js
@@ -1,77 +1,87 @@
-// Global scope result variables for computerSelection & playerSelection.
-let playerResult,
-    computerResult;
+// Global scope variables to hold computerSelection & playerSelection results for use throughout the game.
+let playerSelection,
+    computerSelection;
 
-// Function for accepting player input.
-function playerSelection() {
-
-  // Prompting the player for their choice, converting to lower case, no matter their input.
-  playerResult = prompt("Enter your choice (rock, paper, scissors): ");
-  playerResult = playerResult.toLowerCase();
-
+// Function for accepting player choice.
+function playerPlay() {
+  // Prompting the player for their choice, converting to lower case and removing white space, no matter their input.
+  playerSelection = prompt("Enter your choice (rock, paper, scissors): ");
+  playerSelection = playerSelection.toLowerCase();
+  playerSelection = playerSelection.trim();
   // Verify the player input is valid, notify if not.
-  if (playerResult == "rock" || playerResult == "paper" || playerResult == "scissors") {
-    return playerResult
+  if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
+    return playerSelection
   } else {
-    playerResult = invalid;
-    return "Invalid choice, please choose \"rock\", \"paper\", or \"scissors\""
+    playerSelection = "Invalid choice, please choose \"rock\", \"paper\", or \"scissors\"";
+    return playerSelection
   }
 }
 
-// Function for computer input.
-function computerSelection() {
+// Function for computer choice.
+function computerPlay() {
   // Expression for generating a random number.  Rock, paper, or scissors will be tied to said number.
   const randomSelection = () => Math.floor(Math.random()*3)
-  computerResult = randomSelection();
-
+  computerSelection = randomSelection();
   // Depending on the return of randomSelection, the computer makes its selection.
-  if (computerResult == 0) {
-    return computerResult = "rock";
-  } else if (computerResult == 1) {
-    return computerResult = "paper";
+  if (computerSelection == 0) {
+    return computerSelection = "rock";
+  } else if (computerSelection == 1) {
+    return computerSelection = "paper";
   } else {
-    return computerResult = "scissors";
+    return computerSelection = "scissors";
   }
 }
 
-// Function for playing a round of Rock, Paper, Scissors.
-function playRound() {
-  // Calling the playerSelection & computerSelection functions for their input.
-  playerSelection();
-  computerSelection();
+// Function for playing one round of Rock, Paper, Scissors.
+function playRound(playerSelection, computerSelection) {
   // Expressions for handling the end of round, depending on the outcome.
   let roundDraw = () => {
-    let result = playerResult + " vs " + computerResult + ", is a draw!"
+    let result = playerSelection.toUpperCase() + " vs " + computerSelection.toUpperCase() + ", round is a draw!"
     return result
   };
   let roundWin = () => {
-    let result = roundWin = playerResult + " vs " + computerResult +  ", you win!"
+    let result = roundWin = playerSelection.toUpperCase() + " vs " + computerSelection.toUpperCase() +  ", you win!"
     return result
   };
   let roundLose = () => {
-    let result = roundLose = playerResult + " vs " + computerResult +  ", you lose!";
+    let result = roundLose = playerSelection.toUpperCase() + " vs " + computerSelection.toUpperCase() +  ", you lose!";
     return result
   };
   // Round conditions dictating Win/Loss/Tie.
-  if (playerResult == computerResult) {
+  if (playerSelection == computerSelection) {
     return roundDraw();
-  } else if (playerResult == "rock") {
-    if (computerResult == "paper") {
+  } else if (playerSelection == "rock") {
+    if (computerSelection == "paper") {
       return roundLose();
     } else {
       return roundWin();
     }
-  } else if (playerResult == "paper") {
-    if (computerResult == "scissors") {
+  } else if (playerSelection == "paper") {
+    if (computerSelection == "scissors") {
       return roundLose();
     } else {
       return roundWin();
     }
-  } else if (playerResult == "scissors") {
-    if (computerResult == "rock") {
+  } else if (playerSelection == "scissors") {
+    if (computerSelection == "rock") {
       return roundLose();
     } else {
       return roundWin();
     }
+  }
+}
+
+// Function for calling the game to start.
+function game() {
+  // Calling the playerSelection & computerSelection functions for their input.
+  playerSelection = playerPlay();
+  computerSelection = computerPlay();
+  // Check for invalid player input.
+  if (playerSelection.length > 8) {
+    return playerSelection;
+    // Else resume the game.
+  } else {
+    // Calling one round of play, inputing results of playerSelection & computerSelection.
+    return playRound(playerSelection, computerSelection);
   }
 }
