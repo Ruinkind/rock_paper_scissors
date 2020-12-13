d68c765de34884ac30554a98208eb3070f8cc869
Ruinkind <terroth@gmail.com>
2020-12-13 15:54:44 -0700

    Adjust redundant comments.  Adjust round detection for invalid input.  Add round counter display.

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-// Global variables to hold computerSelection & playerSelection results for use throughout the game.
 let playerSelection,
     computerSelection,
-// Global variables for recording the game score.
     scorePlayer = 0,
     scoreComputer = 0,
     scoreDraw = 0;
 
-// Function for accepting player choice.
 function playerPlay() {
-  // Boolean for controlling the while loop, waiting for proper player input.
   let waiting = true;
   while (waiting) {
-    // Prompting the player for their choice.
-    playerSelection = prompt("Enter your choice (rock, paper, scissors): ")
-    // If cancelled, game ends.
+    playerSelection = prompt("Enter your choice (rock, paper, scissors): ", "rock")
+
     if (playerSelection === null) {
       alert("Game cancelled.")
       return playerSelection;
     } else {
-      // Convert player input to lower case & trim white space for easier handling.
-      playerSelection = playerSelection.toLowerCase()
-      playerSelection = playerSelection.trim()
+      playerSelection = playerSelection.toLowerCase();
+      playerSelection = playerSelection.trim();
       // Verify the player input is valid, notify if not.
       if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
         return playerSelection;
@@ -31,12 +25,10 @@ function playerPlay() {
   }
 }
 
-// Function for computer choice.
 function computerPlay() {
-  // Expression for generating a random number.  Rock, paper, or scissors will be tied to said number.
+  // Generate a random number.  Rock, paper, or scissors will be tied to said number.
   const randomSelection = () => Math.floor(Math.random()*3)
   computerSelection = randomSelection();
-  // Depending on the return of randomSelection, the computer makes its selection.
   if (computerSelection == 0) {
     return computerSelection = "rock";
   } else if (computerSelection == 1) {
@@ -46,9 +38,8 @@ function computerPlay() {
   }
 }
 
-// Function for playing one round of Rock, Paper, Scissors.
 function playRound(playerSelection, computerSelection) {
-  // Expressions for handling the end of round, depending on the outcome.
+  // Handling the end of round, depending on the outcome.
   let roundDraw = () => {
     score()
     let result = playerSelection.toUpperCase() + " vs " + computerSelection.toUpperCase() + ", round is a draw!"
@@ -88,9 +79,7 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-// Function for managing the score.
 function score(result) {
-  // Parameters for requesting score, and resetting the score.
   if (result == "request") {
     let request = "Wins: " + scorePlayer + "  Losses: " + scoreComputer + "  Draws: " + scoreDraw;
     return request;
@@ -99,7 +88,6 @@ function score(result) {
     scoreComputer = 0;
     scoreDraw = 0;
     return score("request");
-    // Depending on the parameter input, the score increments.
   } else if (result == playerSelection) {
     scorePlayer++;
   } else if (result == computerSelection) {
@@ -109,31 +97,37 @@ function score(result) {
   }
 }
 
-// Function for calling the game to start.
 function game() {
-  // Prompt the player for the amount of rounds & call the gameStart function.
-  let round = prompt("How many rounds would you like to play?  Enter a number. ");
-  // If cancelled, game ends.
+  let round = prompt("How many rounds would you like to play?  Enter a number. ", 1);
+
+  let roundCount = (round) => {
+    for (let i = 0; i < round; i++) {
+      console.log("Round: " + round);
+    }
+  }
+
   if (round === null) {
     console.log("Game cancelled.");
     alert("Game cancelled.");
     return;
+  } else if (round == 0) {
+    return alert("Please enter a valid number of rounds.");
   }
+
   gameStart(round);
-  // Starts the game with specified rounds.
+
   function gameStart(round) {
-    // Calling for player input & computer input.
     let roundInput = () => {
       playerSelection = playerPlay()
       computerSelection = computerPlay()
     };
-    // If there are rounds specified, loop until satisfied.
+    
+    // Loop until satisfied the number of rounds.
     if (round) {
       score("reset");
       while (round) {
-        console.log(round);
+        roundCount(round);
         roundInput();
-        // If cancelled, game ends.
         if (playerSelection === null) {
           console.log("Game cancelled.");
           break;
